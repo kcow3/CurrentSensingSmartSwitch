4a97c96e7f6c533f731789a95d7519518f70e409
kcow3 <cowdrey.kevin@gmail.com>
2021-05-29 18:32:27 +0200

    Add code to allow for serial comms for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,53 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Global variables
+bool shouldPrintDebug = true;
+
+// Declerations
+void serialSetup(int baudRate);
+void serialPrintDebug(String s);
+void serialPrintLineDebug(String s);
+
+void setup()
+{
+  serialSetup(115200);
+  
+  serialPrintLineDebug("Board setup complete...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+}
+
+void serialSetup(int baudRate)
+{
+  Serial.begin(baudRate);
+  serialPrintLineDebug("");
+  serialPrintLineDebug("Serial setup done.");
+}
+
+/**
+ * Prints a string to the console. 
+ * A global debug flag is checked to determine if output should be generated.
+ *
+ * @param s The string that will be printed to the console
+ * @return void
+ */
+void serialPrintDebug(String s)
+{
+  if (shouldPrintDebug)
+    Serial.print(s);
+}
+
+/**
+ * Prints a string to the console and appends a new line. 
+ * A global debug flag is checked to determine if output should be generated.
+ *
+ * @param s The string that will be printed to the console
+ * @return void
+ */
+void serialPrintLineDebug(String s)
+{
+  if (shouldPrintDebug)
+    Serial.println(s);
 }
